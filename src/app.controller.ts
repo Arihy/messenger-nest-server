f60792b80d158177a418a9df9b41dfea2d992b8b
Mellany <ynallem@gmail.com>
2020-09-14 23:14:14 +0200

    Refacto appcontroller pour utiliers @Body au mieux de @Request pour les routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { TokenDTO } from './auth/dto/token-dto.class';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
@@ -10,13 +10,13 @@ export class AppController {
   constructor(private authService: AuthService) {}
 
   @Post('auth/signup')
-  async signup(@Request() req: any): Promise<User> {
-    return this.authService.register(req.body as UserCredencialsDTO);
+  async signup(@Body() userCredencials: UserCredencialsDTO): Promise<User> {
+    return this.authService.register(userCredencials);
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req: any): Promise<TokenDTO> {
-    return this.authService.login(req.user);
+  async login(@Body() userCredencials: UserCredencialsDTO): Promise<TokenDTO> {
+    return this.authService.login(userCredencials);
   }
 }
